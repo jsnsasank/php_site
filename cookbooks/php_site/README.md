cc0af84a177f14b886c4f768ba4f5868aba3d8a6
sasank <sasank@sasank.com>
2016-12-04 04:12:10 +0000

    initial commit of php site

---
+++ cookbooks/php_site/README.md
@@ -0,0 +1,4 @@
+# php_site
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/php_site/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: php_site
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
